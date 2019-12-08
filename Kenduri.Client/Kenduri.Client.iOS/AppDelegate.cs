14a9c89e8a18d3a7b6f4cbbd28df7accadeea43c
anas_amir_nur_rashid <anas_amir_nur_rashid@dell.com>
2019-12-08 14:38:02 +0800

    Implemented Material Design
    MainPage base layout; Added CateringListPage;

--- Kenduri.Client/Kenduri.Client.iOS/AppDelegate.cs
+++ Kenduri.Client/Kenduri.Client.iOS/AppDelegate.cs
@@ -23,6 +23,7 @@ namespace Kenduri.Client.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            global::Xamarin.Forms.FormsMaterial.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
